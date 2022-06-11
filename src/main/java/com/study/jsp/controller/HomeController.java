1f98530e93d869c455e7ec9696d7067aa4c5b2ff
RyuGeun <h46344@naver.com>
2022-06-12 02:01:27 +0900

    feat : 내용수정

--- src/main/java/com/study/jsp/controller/HomeController.java
+++ src/main/java/com/study/jsp/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.study.jsp.vo.ActorVO;
@@ -85,57 +86,57 @@ public class HomeController {
 	}
 	
 	
-	@GetMapping("/board")
-	public String loadBoardPage(ModelMap map, HttpSession httpSession) {
-		
-		Map<String, Object> data = new HashMap<String, Object>();
-		
-		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		
-		Map<String, Object> map1 = new HashMap<String, Object>();
-		map1.put("Company", "Alfreds Futterkiste");
-		map1.put("Contact", "Maria Anders");
-		map1.put("Country", "Germany");
-		
-		Map<String, Object> map2 = new HashMap<String, Object>();
-		map2.put("Company", "Centro comercial Moctezuma");
-		map2.put("Contact", "Francisco Chang");
-		map2.put("Country", "Mexico");
-		
-		Map<String, Object> map3 = new HashMap<String, Object>();
-		map3.put("Company", "Ernst Handel");
-		map3.put("Contact", "Roland Mendel");
-		map3.put("Country", "Austria");
-		
-		list.add(map1);
-		list.add(map2);
-		list.add(map3);
-		
-		data.put("list", list);
-		data.put("size", 3);
-		
-		map.addAttribute("data", data);
-		
-		
-		// 모든 메소드에 Session을 확인하고 싶을 때는 데이터 요청을 받기 전 Interceptor에서 미리 받아서 확인한다.
-		// "/login"에서 SET한 Session데이터 가져오기
-		String userId = (String)httpSession.getAttribute("userId");
-		String userPassword = (String)httpSession.getAttribute("userPassword");
-		
-		System.out.println("Session에서 가져온 데이터 "+userId);
-		
-		// 해석 : userId가 null이라는것은 login을 하지 않았다는 의미.
-		// redirect(재요청, 다시 돌아가라)
-		if(userId == null) { 
-			// if가 실행되면 "/login"으로 return
-			return "redirect:/login";
-		}
-		map.addAttribute("userNo", 255);
-		map.addAttribute("userId", userId);
-		
-		return "board";
-	}
-	
+//	@GetMapping("/board")
+//	public String loadBoardPage(ModelMap map, HttpSession httpSession) {
+//		
+//		Map<String, Object> data = new HashMap<String, Object>();
+//		
+//		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+//		
+//		Map<String, Object> map1 = new HashMap<String, Object>();
+//		map1.put("Company", "Alfreds Futterkiste");
+//		map1.put("Contact", "Maria Anders");
+//		map1.put("Country", "Germany");
+//		
+//		Map<String, Object> map2 = new HashMap<String, Object>();
+//		map2.put("Company", "Centro comercial Moctezuma");
+//		map2.put("Contact", "Francisco Chang");
+//		map2.put("Country", "Mexico");
+//		
+//		Map<String, Object> map3 = new HashMap<String, Object>();
+//		map3.put("Company", "Ernst Handel");
+//		map3.put("Contact", "Roland Mendel");
+//		map3.put("Country", "Austria");
+//		
+//		list.add(map1);
+//		list.add(map2);
+//		list.add(map3);
+//		
+//		data.put("list", list);
+//		data.put("size", 3);
+//		
+//		map.addAttribute("data", data);
+//		
+//		
+//		// 모든 메소드에 Session을 확인하고 싶을 때는 데이터 요청을 받기 전 Interceptor에서 미리 받아서 확인한다.
+//		// "/login"에서 SET한 Session데이터 가져오기
+//		String userId = (String)httpSession.getAttribute("userId");
+//		String userPassword = (String)httpSession.getAttribute("userPassword");
+//		
+//		System.out.println("Session에서 가져온 데이터 "+userId);
+//		
+//		// 해석 : userId가 null이라는것은 login을 하지 않았다는 의미.
+//		// redirect(재요청, 다시 돌아가라)
+//		if(userId == null) { 
+//			// if가 실행되면 "/login"으로 return
+//			return "redirect:/login";
+//		}
+//		map.addAttribute("userNo", 255);
+//		map.addAttribute("userId", userId);
+//		
+//		return "board";
+//	}
+//	
 	//  / : ROOT경로 
 	// ex) /board/boardId/12
 	@GetMapping("/")
@@ -188,6 +189,15 @@ public class HomeController {
 		
 	}
 	
+	
+	@GetMapping("/board")
+	public String callBoardPage(ModelMap map,
+			@RequestParam("pageNum") int pageNum,
+			@RequestParam("pageSize") int pageSize
+			){
+		
+		return "board";
+	}
 	
 	
 	
